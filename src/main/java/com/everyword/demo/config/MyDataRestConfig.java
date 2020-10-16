0922129e6ad8e30ef7bcfc033ccc515ac5963172
KonstantinKnaub <constantine.dfb@gmail.com>
2020-10-16 17:06:58 -0400

    adding config class

--- src/main/java/com/everyword/demo/config/MyDataRestConfig.java
+++ src/main/java/com/everyword/demo/config/MyDataRestConfig.java
@@ -1,4 +1,27 @@
 package com.everyword.demo.config;
 
-public class MyDataRestConfig {
+import com.everyword.demo.entities.Phrase;
+import com.everyword.demo.entities.VocabularySet;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.DELETE, HttpMethod.POST};
+
+        config.getExposureConfiguration()
+                .forDomainType(Phrase.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+        config.getExposureConfiguration()
+                .forDomainType(VocabularySet.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+    }
 }
